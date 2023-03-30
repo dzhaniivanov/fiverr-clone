15691c41b18d5d86bda53da5e3e0469c9417f5cc
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-03-30 17:15:03 +0300

    slider with categoryCard added

---
+++ src/components/categoryCard/CategoryCard.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import "./CategoryCard.scss";
+
+const CategoryCard = ({ card }) => {
+  return (
+    <Link to="/gigs?cat=design">
+      <div className="categoryCard">
+        <img src={card.img} alt="item image" />
+        <span className="desc">{card.desc}</span>
+        <span className="title">{card.title}</span>
+      </div>
+    </Link>
+  );
+};
+export default CategoryCard;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,19 @@
+import CategoryCard from "../../components/categoryCard/CategoryCard";
 import Featured from "../../components/featured/Featured";
+import Slide from "../../components/slide/Slide";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import { cards } from "../../data";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CategoryCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
   );
 };
