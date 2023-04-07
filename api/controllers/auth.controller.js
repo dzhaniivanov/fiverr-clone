7fe804e6def4da86fec9cbeac8a83fd8c9d85f66
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-04-07 16:05:45 +0300

    separate error handling added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
 
-export const register = async (req, res) => {
+export const register = async (req, res, next) => {
   try {
     const hash = bcrypt.hashSync(req.body.password, 5);
     const newUser = User({
@@ -13,16 +14,18 @@ export const register = async (req, res) => {
     await newUser.save();
     res.status(201).send("user has been created");
   } catch (error) {
-    res.status(500).send("Something went wrong");
+    next(error);
   }
 };
-export const login = async (req, res) => {
+export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if (!user) return res.status(404).send("User not found");
+
+    if (!user) return next(createError(404, "User not found!"));
 
     const isCorrect = bcrypt.compare(req.body.password, user.password);
-    if (!isCorrect) return res.status(400).send("Wrong password or username");
+    if (!isCorrect) return;
+    next(createError(400, "Wrong password or username"));
 
     const token = jwt.sign(
       {
@@ -36,7 +39,7 @@ export const login = async (req, res) => {
 
     res.cookie("accessToken", token, { httpOnly: true }).status(200).send(info);
   } catch (error) {
-    res.status(500).send("Something went wrong");
+    next(error);
   }
 };
 
--- api/middleware/jwt.js
+++ api/middleware/jwt.js
@@ -1,10 +1,13 @@
-export const verifyToken = (req, res) => {
+import createError from "../utils/createError.js";
+
+export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
-  if (!token) return res.status(401).send("You are not authenticated");
+  if (!token) return next(createError(401, "You are not authenticated!"));
 
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-    if (err) return res.status(403).send("Token isn't valid");
+    if (err) return next(createError(403, "Token isn't valid!"));
     req.userId = payload.id;
     req.isSeller = payload.isSeller;
+    next();
   });
 };
