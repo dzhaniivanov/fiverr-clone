66f62391822b902875b99e7a40615cea4777cefa
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-04-25 17:20:54 +0300

    work on orders & messages component

--- client/src/pages/messages/Messages.tsx
+++ client/src/pages/messages/Messages.tsx
@@ -1,76 +1,87 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import newRequest from "../../utils/newRequest";
 import "./Messages.scss";
-
+import moment from "moment";
 const Messages = () => {
-  // const currentUser = {
-  //   id: 1,
-  //   username: "John Doe",
-  //   isSeller: true,
-  // };
-  const message = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, exercitationem!`;
+  const queryClient = useQueryClient();
+
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["conversations"],
+    queryFn: () => newRequest(`/conversations`).then((res) => res.data),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      return newRequest.put(`/conversationS/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["conversations"]);
+    },
+  });
+
+  const handleRead = (id) => {
+    mutation.mutate(id);
+  };
+
   return (
     <div className="messages">
-      <div className="container">
-        <div className="title">
-          <h1>Messages</h1>
+      {isLoading ? (
+        "Loading..."
+      ) : error ? (
+        "Something went wrong..."
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>Messages</h1>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Buyer</th>
+                <th>Last Message</th>
+                <th>Date</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((conversation) => (
+                <tr
+                  className={
+                    ((currentUser.isSeller && conversation.readBySeller) ||
+                      (!currentUser.isSeller && !conversation.readByBuyer)) &&
+                    "active"
+                  }
+                  key={conversation.id}
+                >
+                  <td>
+                    {currentUser.isSeller
+                      ? conversation.buyerId
+                      : conversation.sellerId}
+                  </td>
+                  <td>
+                    <Link to={`/message/${conversation.id}`} className="link">
+                      {conversation?.lastMessage?.substring(0, 100)}...
+                    </Link>
+                  </td>
+                  <td>{moment(conversation.updatedAt).fromNow()}</td>
+                  <td>
+                    {(currentUser.isSeller && conversation.readBySeller) ||
+                      (!currentUser.isSeller && !conversation.readByBuyer && (
+                        <button onClick={() => handleRead(conversation.id)}>
+                          Mark as Read
+                        </button>
+                      ))}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Buyer</th>
-              <th>Last Message</th>
-              <th>Date</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="active">
-
-              <td>John Doe</td>
-              <td>
-                <Link to="/message/123" className="link">
-                  {message.substring(0, 100)}...
-                </Link>
-              </td>
-              <td>1 day ago</td>
-              <td>
-                <button>Mark as Read</button>
-              </td>
-            </tr>
-            <tr className="active">
-              <td>John Doe</td>
-              <td>
-                <Link to="/message/123" className="link">
-                  {message.substring(0, 100)}...
-                </Link>
-              </td>
-              <td>1 day ago</td>
-              <td>
-                <button>Mark as Read</button>
-              </td>
-            </tr>
-            <tr>
-              <td>John Doe</td>
-              <td>
-                <Link to="/message/123" className="link">
-                  {message.substring(0, 100)}...
-                </Link>
-              </td>
-              <td>1 day ago</td>
-            </tr>
-            <tr>
-              <td>John Doe</td>
-              <td>
-                <Link to="/message/123" className="link">
-                  {message.substring(0, 100)}...
-                </Link>
-              </td>
-              <td>1 day ago</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div >
+      )}
+    </div>
   );
 };
 export default Messages;
--- client/src/pages/orders/Orders.tsx
+++ client/src/pages/orders/Orders.tsx
@@ -1,93 +1,53 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import newRequest from "../../utils/newRequest";
 import "./Orders.scss";
 
 const Orders = () => {
-  const currentUser = {
-    id: 1,
-    username: "John Doe",
-    isSeller: true,
-  };
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["orders"],
+    queryFn: () => newRequest(`/orders`).then((res) => res.data),
+  });
+
   return (
     <div className="orders">
-      <div className="container">
-        <div className="title">
-          <h1>Orders</h1>
+      {isLoading ? (
+        "Loading..."
+      ) : error ? (
+        "Something went wrong..."
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>Orders</h1>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Contact</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((order) => (
+                <tr key={order._id}>
+                  <td>
+                    <img className="img" src={order.img} alt="" />
+                  </td>
+                  <td>{order.title}</td>
+                  <td>{order.price}</td>
+                  <td>
+                    <img className="delete" src="/img/message.png" alt="" />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Title</th>
-              <th>Price</th>
-              <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
-              <th>Contact</th>
-            </tr>
-          </thead>
-          <tbody>
-
-            <tr>
-              <td>
-                <img
-                  className="img"
-                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-              </td>
-              <td>Gig1</td>
-              <td>88</td>
-              <td>123</td>
-              <td>
-                <img className="delete" src="/img/message.png" alt="" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="img"
-                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-              </td>
-              <td>Gig1</td>
-              <td>88</td>
-              <td>123</td>
-              <td>
-                <img className="delete" src="/img/message.png" alt="" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="img"
-                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-              </td>
-              <td>Gig1</td>
-              <td>88</td>
-              <td>123</td>
-              <td>
-                <img className="delete" src="/img/message.png" alt="" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="img"
-                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-              </td>
-              <td>Gig1</td>
-              <td>88</td>
-              <td>123</td>
-              <td>
-                <img className="delete" src="/img/message.png" alt="" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      )}
     </div>
   );
 };
