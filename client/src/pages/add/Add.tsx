44d5b2760e625abf1199580209645c69913214c6
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-05-03 15:31:55 +0300

    add gig with reducer added

--- client/src/pages/add/Add.tsx
+++ client/src/pages/add/Add.tsx
@@ -1,6 +1,50 @@
+import { useReducer, useState } from "react";
+import { gigReducer, INITIAL_STATE } from "../../reducers/gigReducer";
+import upload from "../../utils/upload";
 import "./Add.scss";
 
 const Add = () => {
+  const [singleFile, setSingleFile] = useState(undefined);
+
+  const [files, setFiles] = useState([]);
+  const [uploading, setUploading] = useState(false);
+
+  const [state, dispatch] = useReducer(gigReducer, INITIAL_STATE);
+
+  const handleChange = (e) => {
+    dispatch({
+      type: "CHANGE_INPUT",
+      payload: { name: e.target.name, value: e.target.value },
+    });
+  };
+
+  const handleFeature = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_FEATURE",
+      payload: e.target[0].value,
+    });
+    e.target[0].value = "";
+  };
+  const handleUpload = async () => {
+    setUploading(true);
+
+    try {
+      const cover = await upload(singleFile);
+
+      const images = await Promise.all(
+        [...files].map(async (file) => {
+          const url = await upload(file);
+          return url;
+        })
+      );
+      setUploading(false);
+      dispatch({ type: "ADD_IMAGES", payload: { cover, images } });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="add">
       <div className="container">
@@ -11,25 +55,40 @@ const Add = () => {
             <input
               type="text"
               placeholder="e.g. I will do something I'm really good at"
+              onChange={handleChange}
+              name="title"
             />
             <label htmlFor="">Category</label>
-            <select name="cats" id="cats">
+            <select name="category" id="category" onChange={handleChange}>
               <option value="design">Design</option>
               <option value="web">Web Development</option>
               <option value="animation">Animation</option>
               <option value="music">Music</option>
             </select>
-            <label htmlFor="">Cover Image</label>
-            <input type="file" />
-            <label htmlFor="">Upload Images</label>
-            <input type="file" multiple />
+            <div className="images">
+              <div className="imagesInputs">
+                <label htmlFor="">Cover Image</label>
+                <input
+                  type="file"
+                  onChange={(e) => setSingleFile(e.target.files[0])}
+                />
+                <label htmlFor="">Upload Images</label>
+                <input
+                  type="file"
+                  multiple
+                  onChange={(e) => setFiles(e.target.files)}
+                />
+                <button>{uploading ? "Uploading.." : "Upload"}</button>
+              </div>
+            </div>
             <label htmlFor="">Description</label>
             <textarea
-              name=""
+              name="desc"
               id=""
               placeholder="Brief descriptions to introduce your service to customers"
               cols={0}
               rows={16}
+              onChange={handleChange}
             ></textarea>
             <button>Create</button>
           </div>
@@ -38,23 +97,32 @@ const Add = () => {
             <input type="text" placeholder="e.g. One-page web design" />
             <label htmlFor="">Short Description</label>
             <textarea
-              name=""
+              name="shortDesc"
+              onChange={handleChange}
               id=""
               placeholder="Short description of your service"
               cols={30}
               rows={10}
             ></textarea>
             <label htmlFor="">Delivery Time (e.g. 3 days)</label>
-            <input type="number" />
+
+            <input type="number" name="deliveryTime" onChange={handleChange} />
             <label htmlFor="">Revision Number</label>
-            <input type="number" />
+            <input
+              type="number"
+              name="revisionNumber"
+              onChange={handleChange}
+            />
             <label htmlFor="">Add Features</label>
-            <input type="text" placeholder="e.g. page design" />
-            <input type="text" placeholder="e.g. file uploading" />
-            <input type="text" placeholder="e.g. setting up a domain" />
-            <input type="text" placeholder="e.g. hosting" />
+            <form action="" onSubmit={handleFeature} className="add">
+              <input type="text" placeholder="e.g. page design" />
+              <input type="text" placeholder="e.g. file uploading" />
+              <input type="text" placeholder="e.g. setting up a domain" />
+              <input type="text" placeholder="e.g. hosting" />
+              <button type="submit">add</button>
+            </form>
             <label htmlFor="">Price</label>
-            <input type="number" />
+            <input type="number" onChange={handleChange} name="price" />
           </div>
         </div>
       </div>
--- client/src/pages/myGigs/MyGigs.tsx
+++ client/src/pages/myGigs/MyGigs.tsx
@@ -1,90 +1,80 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import getCurrentUser from "../../utils/getCurrentUser";
+import newRequest from "../../utils/newRequest";
 import "./MyGigs.scss";
 const MyGigs = () => {
+  const currentUser = getCurrentUser();
+
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["myGigs"],
+    queryFn: () =>
+      newRequest(`/gigs?userId=${currentUser.id}`).then((res) => res.data),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      return newRequest.delete(`/gigs/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["myGigs"]);
+    },
+  });
+
+  const handleDelete = (id) => {
+    mutation.mutate(id);
+  };
+
   return (
     <div className="myGigs">
-      <div className="container">
-        <div className="title">
-          <h1>Gigs</h1>
-          <Link to="/add">
-            <button>Add New Gig</button>
-          </Link>
+      {isLoading ? (
+        "Loading..."
+      ) : error ? (
+        "Something went wrong..."
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>Gigs</h1>
+            <Link to="/add">
+              <button>Add New Gig</button>
+            </Link>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Sales</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((gig) => (
+                <tr key={gig._id}>
+                  <td>
+                    <img className="img" src={gig.cover} alt="" />
+                  </td>
+                  <td>{gig.title}</td>
+                  <td>{gig.price}</td>
+                  <td>{gig.sales}</td>
+                  <td>
+                    <img
+                      className="delete"
+                      src="/img/delete.png"
+                      alt=""
+                      onClick={() => handleDelete(gig._id)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Image</th>
-              <th>Title</th>
-              <th>Price</th>
-              <th>Sales</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <img
-                  className="img"
-                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-              </td>
-              <td>Gig1</td>
-              <td>88</td>
-              <td>123</td>
-              <td>
-                <img className="delete" src="/img/delete.png" alt="" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="img"
-                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-              </td>
-              <td>Gig1</td>
-              <td>88</td>
-              <td>123</td>
-              <td>
-                <img className="delete" src="/img/delete.png" alt="" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="img"
-                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-              </td>
-              <td>Gig1</td>
-              <td>88</td>
-              <td>123</td>
-              <td>
-                <img className="delete" src="/img/delete.png" alt="" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <img
-                  className="img"
-                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-              </td>
-              <td>Gig1</td>
-              <td>88</td>
-              <td>123</td>
-              <td>
-                <img className="delete" src="/img/delete.png" alt="" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div >
+      )}
+    </div>
   );
 };
 export default MyGigs;
